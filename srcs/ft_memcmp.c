fe1b0c416c9aaf44a2da28fd4ffec9a233fc9331
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-26 17:00:57 +0100

    tests PASSED

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 15:43:34 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/26 16:04:22 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/11/26 16:59:34 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t i;
 
-	if (n == 0)
-		return (0);
-	i = -1;
-	while (++i < n)
+	i = 0;
+	while (i < n)
 	{
-		if (!(((unsigned char *)s1)[i] == ((unsigned char *)s2)[i]))
-			break ;
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
 	}
-	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	return (0);
 }
