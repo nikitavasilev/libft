87a252d83b7d55bb48dd806ef4b7d12b9b110b53
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-26 17:39:34 +0100

    fix same strs

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 17:50:15 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/24 14:37:47 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/11/26 17:32:02 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int i;
+	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
-	{
-		if (s1[i] != s2[i])
-			break ;
+	while (s1[i] && s2[i] && i < n && s1[i] == s2[i])
 		i++;
-	}
+	if (n == i)
+		return (0);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
