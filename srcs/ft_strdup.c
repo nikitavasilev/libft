69e3c40f5c84cfc0bc3d8a7df08b43ac94791c18
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-27 13:57:56 +0100

    tests OK

---
+++ srcs/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/27 13:43:39 by nvasilev          #+#    #+#             */
+/*   Updated: 2020/11/27 13:56:50 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*copy;
+
+	copy = malloc(ft_strlen(s1) + 1);
+	if (!copy)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i] = s1[i];
+	return (copy);
+}
