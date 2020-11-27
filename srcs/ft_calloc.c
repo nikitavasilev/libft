9b2c6fceba02d56045f5d36711d1335683e3d9ab
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-27 11:59:13 +0100

    fix size 0

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 11:45:40 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/24 14:36:27 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/11/27 11:58:28 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void *arr;
 
-	if (!count || !size)
-		return (NULL);
 	arr = malloc(count * size);
 	if (!arr)
 		return (0);
