abb47372fd57a176458d2662406e1a4de611c9e7
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-27 22:37:12 +0100

    tests OK

---
+++ srcs/ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/27 14:02:01 by nvasilev          #+#    #+#             */
+/*   Updated: 2020/11/27 21:11:16 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && haystack[i + j] && needle[j])
+			j++;
+		if (!needle[j])
+		{
+			if (i + j > len)
+				return (0);
+			return ((char *)&haystack[i]);
+		}
+		i++;
+	}
+	return (0);
+}
