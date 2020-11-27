e6c84bdd4a9e04d415534092ba3fa17d35667989
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-27 12:17:43 +0100

    fix cast

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 12:50:51 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/24 14:30:26 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/11/27 12:17:05 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*s;
-	char	*d;
 
-	s = (char *)src;
-	d = (char *)dst;
+	if (!src && !dst)
+		return (0);
 	i = 0;
-	while (s[i] && i < (n - 1))
+	while (i < n)
 	{
-		d[i] = s[i];
+		((char *)dst)[i] = ((char *)src)[i];
 		i++;
 	}
-	d[i] = s[i];
 	return (dst);
 }
