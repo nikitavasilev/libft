1fb31fed10265df1b24a5c2b66be97408272503c
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-11-28 13:25:56 +0100

    protected

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 12:19:12 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/28 12:32:49 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/11/28 13:24:47 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
+	if (!dst)
+		return (0);
 	i = 0;
-	while (src[i] && i < dstsize)
+	while (src[i] && i < dstsize - 1 && dstsize > 0)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return(ft_strlen(src));
+	if (dstsize > 0)
+		dst[i] = '\0';
+	return (ft_strlen(src));
 }
