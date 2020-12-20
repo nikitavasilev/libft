64e2c9a803646925f000e8a88ea9da058e30917c
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-12-20 15:12:50 +0100

    norm

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 13:46:27 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/12/20 14:34:52 by nvasilev         ###   ########.fr       */
+/*   Updated: 2020/12/20 15:12:36 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 	size_t	i;
 
-	if(!(str = malloc(len + 1)) || !s)
+	if (!(str = malloc(len + 1)) || !s)
 		return (0);
 	i = 0;
 	while (s[start + i] && i < len && start < ft_strlen(s))
