6a0ca1832f96b8165ac6bd210db20c68fbc81c97
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-01-05 21:23:43 +0100

    fix cast

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 17:21:08 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/12/25 17:47:33 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/01/05 21:22:19 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
+	{
 		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
 	if ((unsigned int)n >= 10)
 		ft_putnbr_fd((unsigned int)n / 10, fd);
 	ft_putchar_fd((unsigned int)n % 10 + '0', fd);
