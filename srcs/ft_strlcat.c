c33db471918dc2f1943f2ac65a5ce120f04a7bad
Nikita VASILEV <nvasilev@student.42.fr>
2021-01-07 16:34:41 +0100

    fix <= instead of <

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 14:14:22 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/11/28 15:06:27 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/01/07 16:26:30 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 
 	dst_len = ft_strlen(dst);
-	if (dstsize < dst_len)
+	if (dstsize <= dst_len)
 		return (dstsize + ft_strlen(src));
 	i = 0;
 	while (src[i] && (dst_len + i) < dstsize - 1)
