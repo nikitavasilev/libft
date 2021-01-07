4e4a943b903f0088655d1620125dad72ea538fbf
Nikita VASILEV <nvasilev@student.42.fr>
2021-01-07 17:34:30 +0100

    add function to free alloc if err

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 17:50:34 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/12/20 20:46:21 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/01/07 17:31:06 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,20 @@ static size_t	count_words(const char *str, char c)
 	return (count);
 }
 
+static char		**free_if_error(char **strs)
+{
+	size_t	i;
+
+	i = 0;
+	while (strs[i])
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (0);
+}
+
 char			**ft_split(char const *s, char c)
 {
 	size_t	i;
@@ -63,7 +77,7 @@ char			**ft_split(char const *s, char c)
 		while (((char *)s)[i] != c && ((char *)s)[i])
 			i++;
 		if (!(res[row] = malloc(i - start + 1)))
-			return (0);
+			return (free_if_error(res));
 		str_fill(res[row], s + start, i - start);
 		row++;
 	}
