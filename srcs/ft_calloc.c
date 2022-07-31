c46ccf714117c453215277a2f3230d1cdff6da09
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-07-31 22:24:17 +0200

    fix floating point excepetion

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,10 +6,11 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 11:45:40 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/07/18 00:14:35 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/07/31 20:26:59 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <limits.h>
 #include "libft.h"
 
@@ -17,11 +18,12 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (count > ULLONG_MAX / size)
+	if (count == 0 || size == 0)
+		return (NULL);
+	if (count >= INT_MAX / size)
 		return (NULL);
 	ptr = malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, count * size);
+	if (ptr)
+		ft_bzero(ptr, count * size);
 	return (ptr);
 }
